cbf5e76460e0341d8b62939a5e7a6bc5adefc659
leparwa <kevin.leparwa@student.moringaschool.com>
2021-08-27 00:38:11 +0300

    make it better

--- app.py
+++ app.py
@@ -9,6 +9,5 @@ def test ():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-    
 if __name__ =='__main__':
     app.run()
