764d377c5416a8e9ce8918e1f09bebb227f2943d
leparwa <kevin.leparwa@student.moringaschool.com>
2021-08-27 04:07:02 +0300

    deploy

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ class Config:
     BOOKS_API_BASE_URL= 'https://www.googleapis.com/books/v1/volumes?q={}'
  
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://leresipitchdb:pitchidea@localhost/pitches'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://leresipitchdb:pitchidea@localhost/pitches'
   
 
     
     
 class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     SECRET_KEY ='lkjelkjdskjdjwjepjdhah'
 
 class DevConfig(Config):
